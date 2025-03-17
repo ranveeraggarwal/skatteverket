c9a5a669c74264d8f6d6c7acf6e19bcf1d8c7d06
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2025-03-17 20:44:07 +0100

    All the stuff

---
+++ README.md
@@ -0,0 +1,12 @@
+Automation script to fill in tax data on Skatteverket
+===
+
+Especially useful if you have foreign taxes.
+
+The tax_data is expected in the following format:
+
+Dividend/Interest,Country,Month,Day,Currency,Amount,TP,TP_Currency,TP_Amount
+
+where TP = Tax Paid, if you already have any tax paid on the amount. The name of the country and the month need to be in Swedish as in the example.
+
+Feel free to contribute with an issue. 
---
+++ utlandska-skatt.py
@@ -0,0 +1,92 @@
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+## Configuration ChangeMe ##
+YEAR = '2024'
+############################
+
+
+# Create a new ChromeOptions object to configure the Chrome browser
+chrome_options = webdriver.ChromeOptions()
+
+# Add the following options to allow automated testing with a real instance of Chrome
+chrome_options.add_argument('--no-sandbox')   # Bypass OS security model
+# Overcome limited resource problems
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')   # Disable GPU acceleration
+# Enable remote debugging
+chrome_options.add_argument('--remote-debugging-port=9222')
+
+# Create a new instance of Chrome browser using the ChromeOptions object
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to the website you want to automate
+driver.get("https://app.skatteverket.se/klient-sifu-segmentering/")
+
+# Wait for the form to load
+wait = WebDriverWait(driver, 10)
+wait.until(EC.presence_of_element_located((By.ID, 'year')))
+
+
+def find_element_by_id(element_id) -> WebElement:
+    wait.until(EC.presence_of_element_located((By.ID, element_id)))
+    return driver.find_element(By.ID, element_id)
+
+
+def select_value_in_dropdown(element_id, value):
+    dropdown = find_element_by_id(element_id)
+    select = Select(dropdown)
+    select.select_by_visible_text(value)
+
+
+def add_entry(row_data):
+    # Dividend or interest?
+    if row_data[0] =="D":
+        find_element_by_id('label-inkomsttyp1').click()
+    elif row_data[0] == "I":
+        find_element_by_id('label-inkomsttyp2').click()
+
+    # Select land in Swedish
+    select_value_in_dropdown('land', row_data[1])
+
+    # Select month
+    select_value_in_dropdown('utbetalningsdatum-manad', row_data[2])
+
+    # Select day
+    select_value_in_dropdown('utbetalningsdatum-dag', row_data[3])
+
+    # Select currency
+    select_value_in_dropdown('beloppUtbetald-currency', row_data[4])
+
+    # Input amount
+    find_element_by_id('beloppUtbetald-amount').send_keys(row_data[5])
+
+    # Have I paid for this already? Yes or no.
+    if row_data[6] =="Y":
+        find_element_by_id('label-isUtlandskSkatt1').click()
+        # How much have I paid for this? Currency and amount
+        select_value_in_dropdown('utlandskSkatt-currency', row_data[7])
+        find_element_by_id('utlandskSkatt-amount').send_keys(row_data[8])
+    elif row_data[6] == "N":
+        find_element_by_id('label-isUtlandskSkatt2').click()
+    
+
+    # Submit this entry
+    find_element_by_id('btn-form-submit').click()
+
+
+# Select year
+select_value_in_dropdown('year', YEAR)
+
+with open('tax_data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        add_entry(row)
+
+# This is arbitrary wait, no element called yolo
+WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.ID, 'yolo')))
